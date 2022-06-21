13781b051b5c4148f9eee14c734bb3b38eaaccd0
manuimfeld <manu_jadir@hotmail.com>
2022-06-21 11:41:26 -0300

    Llamar a la API con la tecla enter + archivos CSS ordenados

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -2,17 +2,17 @@ import React from 'react'
 import '../styles/buscador.css'
 import Logo from './Logo'
 
-
   const Buscador = ({buscar, setBuscar, artistas, setArtistas, setErrores, setCargando}) => {
-  
-  const fetchApi = () => {
-    let url = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=10&artist="+ buscar +"&api_key=7cf6752f52b73ffc2eebf3f0c51dd36c&format=json"
+
+  const fetchApi = (e) => {
+    let url = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=10&artist="+ e +"&api_key=7cf6752f52b73ffc2eebf3f0c51dd36c&format=json"
     fetch(url)
     .then(data=>data.json())
     .then(res=>setArtistas(res.similarartists.artist), setErrores(false))
     .catch(err=> setErrores(true), setArtistas([]))
   }
 
+  /* Seteo el estado para mostrar el componente de carga con ternarias en ListaArtistas.js o Inicio.js */
   const cambiarEstado = () => {
     setCargando(true)
     setTimeout(() => {
@@ -20,14 +20,21 @@ import Logo from './Logo'
     }, 1000);
   }
 
+  /* Con esta función, al presionar enter hago un llamado a la api */
+  const detectket = (e) => {
+    if (e.key === 'Enter' && buscar.length > 0) {
+      fetchApi(buscar)
+      cambiarEstado()
+    }
+  }
+
   return (
     <header>
         <nav>
             <Logo />
             <div className='searchbar-container'>
-            <input type="text" onChange={event => setBuscar(event.target.value)} placeholder="Buscar artista"/>
-            {/* <button onClick={fetchApi}>Buscar</button> */}
-            <button onClick={() => {fetchApi(); cambiarEstado();}}>Buscar</button>
+            <input type="text" onChange={event => setBuscar(event.target.value)} onKeyDown={detectket} placeholder="Buscar artista"/>
+            <button onClick={() => {fetchApi(buscar); cambiarEstado();}}>Buscar</button>
             </div>
         </nav>
     </header>
